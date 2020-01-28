0c68c1c6ca976303b7a19380325e1ca6f8696e5a
Jordyn Kuhn <02jordyn.kuhn@gulllakecs.org>
2020-01-28 05:40:55 -0800

    Add While Loop Nested Inside of a For Loop

--- ControlFlow.py
+++ ControlFlow.py
@@ -4,6 +4,21 @@
 # Date: Double For Loop
 
 for i in range(3):
-    print("\nOuter For Loop " + str(i + 1))
+    print("\nOuter For Loop " + str(i))
     for k in range(4):
-        print("\tInner for loop " + str(k + 1))
+        print("\tInner for loop " + str(k))
+
+
+
+print("\n------------------------------\n")
+
+# Programmer: Jordyn Kuhn
+# Program: 1.21.20
+# Date: While Loop Nested Inside of a For Loop
+
+for i in range(4):
+    print("For Loop: " + str(i))
+    x = i
+    while x >= 0:
+        print("\tWhile Loop: " + str(x))
+        x = x - 1
