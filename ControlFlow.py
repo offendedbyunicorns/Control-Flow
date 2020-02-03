4dc212a5983fe136a123d4bbf5f3b48547428caa
Jordyn Kuhn <02jordyn.kuhn@gulllakecs.org>
2020-02-03 05:34:55 -0800

    Add Greeting

---
+++ ControlFlow.py
@@ -0,0 +1,17 @@
+
+# Date: 2.3.20
+# Greeting Function
+
+# Define Variables Here
+
+name = input("\nWhat is your name: ")
+
+# Define Functions Here
+
+def greeting():
+    print("\nHi There! " + name)
+    print("I hope you've had a decent day today...")
+
+# Call Functions Here
+
+greeting()
