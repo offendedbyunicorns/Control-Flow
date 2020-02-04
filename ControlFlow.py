0b889b3e9cfd894244ddb88c2236b808ed239019
Jordyn Kuhn <02jordyn.kuhn@gulllakecs.org>
2020-02-04 05:03:34 -0800

    Added Functions & Parameters

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,27 +1,41 @@
 
 # Date: 2.3.20
-# Greeting Function
+# Programmer: Jordyn Kuhn
 
 # Define Global Variables Here
 
-name = input("\nWhat is your name: ")
+# name = input("\nWhat is your name: ")
 x = 15
 
 # Define Functions Here
+
+# Greeting Function
 
 
 def greeting():
     print("\nHi There! " + name)
     print("I hope you've had a decent day today...")
 
+# Functions & A Local Variable
+
 
 def printSomething():
     x = 3
     print(x)
 
+# Functions & Parameters
+# Function Name = printNumber with a parameter of age
+
+
+def printNumber(age):
+    print(age)
+
+
 # Call Functions Here
 
 
-greeting()
-printSomething()
-print(x)
+# greeting()
+# printSomething()
+# print(x)
+printNumber(28)
+printNumber(39)
