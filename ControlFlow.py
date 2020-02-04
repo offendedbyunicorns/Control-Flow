487f3fa0230f91f55e9fe09ef467987201f20546
Jordyn Kuhn <02jordyn.kuhn@gulllakecs.org>
2020-02-04 04:48:29 -0800

    Add Functions and Variables
    Variable = printSomething()

--- ControlFlow.py
+++ ControlFlow.py
@@ -2,16 +2,26 @@
 # Date: 2.3.20
 # Greeting Function
 
-# Define Variables Here
+# Define Global Variables Here
 
 name = input("\nWhat is your name: ")
+x = 15
 
 # Define Functions Here
+
 
 def greeting():
     print("\nHi There! " + name)
     print("I hope you've had a decent day today...")
 
+
+def printSomething():
+    x = 3
+    print(x)
+
 # Call Functions Here
 
+
 greeting()
+printSomething()
+print(x)
